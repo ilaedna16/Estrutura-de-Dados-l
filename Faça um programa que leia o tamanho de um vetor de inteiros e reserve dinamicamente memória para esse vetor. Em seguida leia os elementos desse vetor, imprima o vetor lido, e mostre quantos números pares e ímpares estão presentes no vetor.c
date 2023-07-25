5215b2c7dc00ddbfa346dcaefe1e51e539562b96
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2023-07-24 22:37:10 -0300

    Create Faça um programa que leia o tamanho de um vetor de inteiros e reserve dinamicamente memória para esse vetor. Em seguida leia os elementos desse vetor, imprima o vetor lido, e mostre quantos números pares e ímpares estão presentes no vetor.c

---
+++ Faça um programa que leia o tamanho de um vetor de inteiros e reserve dinamicamente memória para esse vetor. Em seguida leia os elementos desse vetor, imprima o vetor lido, e mostre quantos números pares e ímpares estão presentes no vetor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+//Faça um programa que leia o tamanho de um vetor de inteiros e reserve dinamicamente memória para esse vetor.
+//Em seguida leia os elementos desse vetor, imprima o vetor lido, 
+//e mostre quantos números pares e ímpares estão presentes no vetor
+
+int main () { 
+        setlocale (LC_ALL, "portuese"); 
+
+int *p; 
+int n, med;
+     printf ("Digite o tamanho do vetor:\n"); 
+     scanf ("%d", &n); 
+    p = (int*) malloc (n *sizeof (int)); 
+      for (int i=0; i<n;i++){ 
+     scanf ("%d", &p[i]); 
+    med+= p[i];  
+    med=med/n; 
+     printf ("A média é:%.d",med);
+    free(p); 
+      } 
+return 0; 
+}
