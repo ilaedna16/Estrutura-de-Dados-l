3d4db081fe029b740de9566e7a224f9ab8078afe
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2023-08-12 16:17:28 -0300

    Create Faça um programa que receba os dados e imprima o nome dos estudabtes, assim como sua média final, deve ser feita de um arquivo de texto.c

---
+++ Faça um programa que receba os dados e imprima o nome dos estudabtes, assim como sua média final, deve ser feita de um arquivo de texto.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//Escreva aum programa que receba os dados de três estudanteds e imprima na tela o nome dos estudantes que foram aprovados, 
+//assim como sua média final. Cada estudante possui um nome e três notas, referente a cada unidade da disciplina,
+//a leitura dos dados deve ser feita de um arquivo de texto. 
+
+	typedef struct { 
+	char nome[50]; 
+	float n1; 
+	float n2; 
+	float n3; 
+		} estudante;
+int main()
+{
+      estudante e1; 
+       estudante e2;
+        estudante e3;
+             estudante v[3]= {e1, e2, e3};
+             int i;
+			 FILE *file = fopen("./estudantes.txt", "r");    //abrindo o fopen somente para leitura
+             printf ("Informe os dados dos estudantes:\n"); 
+             for (i=0; i<3; i++) { 
+             fscanf ( file "%s %f %f %f", &v[i].nome, &v[i].n1, &v[i].n2, &v[i].n3); 
+			 }
+			  for (i=0; i<3; i++) { 
+			  float media= (v[i].n1 + v[i].n2 + v[i].n3)/3; 
+			  if (media >= 7.0) { 
+			   	  printf ("%s: %.2f\n", v[i].nome, media); 
+			   }  
+			   
+			} 
+			fclose (file); 
+} 
+   
