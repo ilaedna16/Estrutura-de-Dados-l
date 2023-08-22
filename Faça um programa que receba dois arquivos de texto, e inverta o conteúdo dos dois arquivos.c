be4ead153f6456cdcd4d45161fa5eef7f5d9b015
Ilaedna Delmiro <115794672+ilaedna16@users.noreply.github.com>
2023-08-22 13:21:04 -0300

    Create Faça um programa que receba dois arquivos de texto, e inverta o conteúdo dos dois arquivos.c

---
+++ Faça um programa que receba dois arquivos de texto, e inverta o conteúdo dos dois arquivos.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    
+    FILE *A, *B, *C; 
+    char texto; 
+    A= fopen ("A.txt", "r"); 
+    C= fopen ("C.txt", "w"); 
+        while (fscanf (A, "%c", &texto)!= EOF) {
+            fprintf (C, "%c", texto); 
+        } 
+        fclose (A); 
+        fclose (C); 
+        B= fopen ("B.txt", "r"); 
+        A= fopen ("A.txt", "w");
+        
+        while (fscanf (B, "%c", &texto)!= EOF) {
+            fprintf (B, "%c", texto); 
+        } 
+        fclose (B); 
+        fclose (A); 
+        
+        C= fopen ("C.txt", "r"); 
+        B= fopen ("B.txt", "w");
+        while (fscanf (C, "%c", &texto)!= EOF) {
+            fprintf (B, "%c", texto);
+             fclose (B); 
+             fclose (C);
+
+                 remove ("c.txt");
+
+}
+        
+    return 0;
+}
